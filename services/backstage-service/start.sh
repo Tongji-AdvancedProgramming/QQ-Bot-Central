b66ba52326727d26d88295cde126d4740c8f4b4d
Zhang Yao <cineazhan@gmail.com>
2023-09-18 14:17:33 +0800

    fix: start.sh

--- services/backstage-service/start.sh
+++ services/backstage-service/start.sh
@@ -4,4 +4,4 @@
 #echo "172.17.0.1 host.docker.internal" >> /etc/hosts
 
 # Start the Java application
-java -Xmx256m -Djasypt.encryptor.password=rmbrus -jar /root/app.jar
+java -Xmx300m -Djasypt.encryptor.password=rmbrus -jar /root/app.jar
